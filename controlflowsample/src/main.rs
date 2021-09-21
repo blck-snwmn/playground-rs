0df08af7c704070e4c1e4768d6d70f10d346e555
blck-snwmn <whitesnowmancreator@gmail.com>
2021-09-22 02:37:39 +0900

    Add sample

--- controlflowsample/src/main.rs
+++ controlflowsample/src/main.rs
@@ -1,10 +1,20 @@
-use std::ops::ControlFlow;
+use std::ops::{ControlFlow, Range};
 
+fn flow<F>(inputs: Range<i32>, cond: F) -> ControlFlow<i32>
+where
+    F: Fn(i32) -> ControlFlow<i32>,
+{
+    for i in inputs {
+        // ? が使える
+        cond(i)?
+    }
+    ControlFlow::Continue(())
+}
 fn main() {
     {
         // 13 まで表示される
         let r = (2..100).try_for_each(|x| {
-            println!("[{}]", x);
+            println!("[try_for_each]{}", x);
             if 403 % x == 0 {
                 return ControlFlow::Break(x);
             }
@@ -16,7 +26,7 @@ fn main() {
     {
         // 最後まで表示される。
         let r = (2..10).fold(ControlFlow::Continue(()), |acc, x| {
-            println!("[{}]", x);
+            println!("[fold]{}", x);
             if 7 % x == 0 {
                 return ControlFlow::Break(x);
             }
@@ -24,5 +34,25 @@ fn main() {
             ControlFlow::Continue(())
         });
         println!("{:?}", r)
+    }
+    {
+        let r = flow(1..10, |x| {
+            println!("[flow1]{}", x);
+            match x {
+                xx if xx % 5 == 0 => ControlFlow::Break(xx),
+                _ => ControlFlow::Continue(()),
+            }
+        });
+        println!("{:?}", r) // Break(5)
+    }
+    {
+        let r = flow(1..10, |x| {
+            println!("[flow2]{}", x);
+            match x {
+                xx if xx % 100 == 0 => ControlFlow::Break(xx),
+                _ => ControlFlow::Continue(()),
+            }
+        });
+        println!("{:?}", r) // Continue(())
     }
 }
