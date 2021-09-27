c176e6c588d2ef1c6f411b063d8685c0c8b60dd6
blck-snwmn <whitesnowmancreator@gmail.com>
2021-09-28 00:03:46 +0900

    Add output

--- error-sample/src/main.rs
+++ error-sample/src/main.rs
@@ -1,6 +1,12 @@
 use anyhow::{Context, Result};
 fn main() -> Result<()> {
     do_1().context("aaaaaa")
+    // Error: aaaaaa
+    //
+    // Caused by:
+    //     0: with_context in do_1
+    //     1: context in do_2
+    //     2: failed to do_2
 }
 
 fn do_1() -> Result<()> {
